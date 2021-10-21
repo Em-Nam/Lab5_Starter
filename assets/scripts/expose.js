aa084e4cd7ad79bc834a4a349b4230e4d785a3ae
Emmy Nam <emmynam13@gmail.com>
2021-10-21 08:50:11 -0700

    set volume level, set volume icon

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,16 +4,56 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+    const LEVEL_ONE_VOLUME_LIMIT = 33;
+    const LEVEL_TWO_VOLUME_LIMIT = 67;
+
+
+    //functionality when select new horn
     var hornSelect = document.getElementById('horn-select');
     hornSelect.addEventListener('change', function(event){
+        var hornType = event.target.value;
         var img = document.querySelector("[src^='assets/images']");
-        img.src = 'assets/images/' + event.target.value + '.svg';
+        img.src = 'assets/images/' + hornType + '.svg';
+        img.alt = hornType + ' image selected';
 
         console.log(event.target.value);
         console.log(img);
         
         var audio = document.querySelector('audio');
-        audio.src = 'assets/audio/' + event.target.value + '.mp3';
+        audio.src = 'assets/audio/' + hornType + '.mp3';
         console.log(audio);
     })
+
+
+    //functionality when slide volume
+    var volumeSelect = document.getElementById('volume');
+    console.log(volumeSelect);
+
+    volumeSelect.addEventListener('input', function(event){
+        console.log(event.target.value);
+        let volumeValue = event.target.value;
+        var icon = 0;
+        if (volumeValue <= 0){
+            icon = 0;
+        } else if (volumeValue < LEVEL_ONE_VOLUME_LIMIT){
+            icon = 1;
+        } else if (volumeValue < LEVEL_TWO_VOLUME_LIMIT){
+            icon = 2;
+        } else {
+            icon = 3
+        }
+
+        var img = document.querySelector("[src^='assets/icons']");
+        img.src = 'assets/icons/volume-level-' + String(icon) + '.svg';
+        img.alt = 'Volume level ' + String(icon);
+
+        var audio = document.querySelector('audio');
+        audio.volume = volumeValue / 100;
+        console.log(audio.volume);
+    })
+
+    
+
+
 }
